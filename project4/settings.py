31f92e7f4cb86d32dc9281718b176b55bd642f04
Djouko <destructeurkratos@gmail.com>
2023-02-21 03:58:08 +0100

    third commit tradis

--- project4/settings.py
+++ project4/settings.py
@@ -120,5 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'network/media')
+STATIC_ROOT = os.path.join(BASE_DIR, 'network/media')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 MEDIA_URL = '/media/'
