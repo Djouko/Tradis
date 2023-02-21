24ff786c9436ccd0548ec05f91266cc7e52c4899
Djouko <destructeurkratos@gmail.com>
2023-02-21 02:46:48 +0100

    second commit tradis

--- project4/settings.py
+++ project4/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '13kl@xtukpwe&xj2xoysxe9_6=tf@f8ewxer5n&ifnd46+6$%8'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['example.com', 'www.example.com', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
